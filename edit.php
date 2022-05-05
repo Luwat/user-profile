ac97e651605b1fb88694ebd89a2a3c910c96608e
luwat <oluwatobioreniyi@gmail.com>
2022-05-05 10:53:08 +0100

    delete.php, edit.php and index.php updated. work done!

---
+++ edit.php
@@ -0,0 +1,66 @@
+<?php
+    require 'db.php';
+    $id = $_GET['id'];
+    $sql = 'SELECT * FROM profile WHERE id=:id';
+    $statement = $connection->prepare($sql);
+    $statement->execute([':id' => $id]);
+    $user = $statement->fetch(PDO::FETCH_OBJ);
+
+
+    if (isset($_POST['name']) && isset($_POST['address']) && 
+        isset($_POST['email']) && 
+            isset($_POST['phone_number'])) {
+                $name = $_POST['name'];
+                $address = $_POST['address'];
+                $email = $_POST['email'];
+                $phone_number = $_POST['phone_number'];
+
+                $sql = 'UPDATE profile SET name=:name, address=:address, 
+                email=:email, phone_number=:phone_number WHERE id=:id';
+
+                $statement = $connection->prepare($sql);
+                if ($statement->execute([':name' => $name, ':address' => $address, ':email' => $email, ':phone_number' => $phone_number, ':id' => $id])) {
+                    header('Location: /user-profile/');
+                }
+            }
+?>
+
+<?php require 'header.php'; ?>
+    <div class="container">
+        <div class="card mt-5">
+            <div class="card-header ">
+                <h2>Update Profile</h2>
+            </div>
+            <div class="card-body">
+                <form  method="post">
+                    <div class="form-group">
+                        <label for="name">Name:</label>
+                        <input type="text" value="<?= $user->name; ?>" name="name" id="name" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="address">Address:</label>
+                        <input type="text" value="<?= $user->address; ?>" name="address" id="address" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="email" value="<?= $user->email; ?>" name="email" id="email" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="phone-number">Phone Number:</label>
+                        <input type="number" value="<?= $user->phone_number; ?>" name="phone_number" id="phone_number" class="form-control">
+                    </div>
+                    <div class="form-group mt-3">
+                        <button type="submit" class="btn btn-info">Update Profile</button>
+                        
+                    </div>
+                </form>
+                <?php if (!empty($message)): ?>
+                <div class="alert alert-success">
+                    <?= $message; ?>
+                </div>
+                <?php endif; ?>
+            </div>
+        </div>
+        
+    </div>
+<?php require 'footer.php'; ?>
--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<?php
+    require 'db.php';
+    $sql = 'SELECT * FROM profile';
+    $statement = $connection->prepare($sql);
+    $statement->execute();
+    $profile = $statement->fetchAll(PDO::FETCH_OBJ);
+
+?>
 <?php require 'header.php'; ?>
-    
+    <div class="container">
+        <div class="card mt-5">
+            <div class="card-header">
+                <h2>All Users</h2>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered">
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Address</th>
+                        <th>Email</th>
+                        <th>Phone Number</th>
+                        <th>Action</th>
+                    </tr>
+                    <?php foreach($profile as $user): ?>
+                    <tr>
+                        <td><?= $user->id ;?></td>
+                        <td><?= $user->name ;?></td>
+                        <td><?= $user->address ;?></td>
+                        <td><?= $user->email ;?></td>
+                        <td><?= $user->phone_number ;?></td>
+                        <td>
+                            <a href="edit.php?id=<?= $user->id; ?>" class="btn btn-info">Edit</a>
+                            <a onclick="return confirm('Are you sure you want to delete this user\'s profile?')" href="delete.php?id=<?= $user->id; ?>" class="btn btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
+                </table>
+            </div>
+        </div>
+    </div>
 <?php require 'footer.php'; ?>
